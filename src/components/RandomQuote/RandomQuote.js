7b04cbcccba19aeacd873fe7000f0bebd2bd41a3
Bob Murmu <bobmurmu02@gmail.com>
2023-10-18 16:12:40 +0530

    First Commit

--- src/components/RandomQuote/RandomQuote.js
+++ src/components/RandomQuote/RandomQuote.js
@@ -1,3 +1,4 @@
+import React, {useState, useEffect} from 'react';
 import './RandomQuote.css';
 
 function random(min, max){
@@ -10,50 +11,65 @@ function randomColor(){
     return rc;
 }
 
-async function handleClick(){
-    const quotes = await getQuote();
-    const quote = quotes[random(0,quotes.length-1)];
-    const text = document.getElementById("text");
-    const textIcon = text.querySelector("i").outerHTML;
-    text.innerHTML = textIcon + `${quote.text}`;
-    const author = quote.author.split(',')[0];
-    document.getElementById("author").textContent = `- ${author}`;
-    const root = document.querySelector(':root');
-    root.style.setProperty('--color', randomColor());
-}
-
-async function getQuote(){
-    const response = await fetch("https://type.fit/api/quotes");
-    const data = await response.json();
-    return data;
-}
-
-function handleTweet(){
-    const quoteText = document.getElementById("text").textContent;
-    const quoteAuthor = document.getElementById("author").textContent;
-    const tweetText = `${quoteText}  -${quoteAuthor}`;
-    const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetText)}`;
-    window.open(tweetUrl, '_blank');
-}
-
 export default function RandomQuote(){
+    const[data, setQuote] = useState(null);
+
+    useEffect(()=>{
+        getQuote();
+    },[]);
+    
+    async function getQuote(){
+        try{
+            const response = await fetch("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json",{
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json'
+                }
+            });
+            if(!response.ok){
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            console.log(data);
+            setQuote(getRandomQuote(data));
+            const newColor = randomColor();
+            document.documentElement.style.setProperty('--color', newColor);
+        }catch(error){
+            console.error(error);
+            console.log('Oops... Something went wrong');
+        }
+    }
+    function getRandomQuote(data){
+        return data.quotes[
+            Math.floor(Math.random()* data.quotes.length)
+        ]
+    }
+    
+    function handleTweet(){
+        const tweetText = `${data.quote}  -${data.author}`;
+        const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetText)}`;
+        return tweetUrl;
+    }
+
+    if(!data) return null;
+
     return(
         <div id="quote-box" className="d-flex flex-column w-100 bg-light p-5 rounded-5">
             <h1 id="text" className="fs-3 text-primary">
                 <i className="fa-solid fa-quote-left me-3 fs-1 text-primary"></i>
-                Whatever you can do, or dream you can, begin it. Boldness has genius, power and magic in it.
+                {data.quote}
             </h1>
         
             <h1 id="author" className="align-self-end fs-4 text-primary">
-            - Johann Wolfgang von Goethe
+            - {data.author}
             </h1>
             <div className="footer mt-4 d-flex justify-content-between align-items-center">
                 <div className="share">
-                    <a onClick={handleTweet}  target="_blank" id="tweet" className="bg-primary py-2 px-3 rounded-2">
+                    <a href={handleTweet()} target='_blank' rel='noreferrer' id="tweet-quote" className="bg-primary py-2 px-3 rounded-2">
                         <i className="fa-brands fa-twitter text-light"></i>
                     </a>
                 </div>
-                <button id="new-quote" className=" btn btn-default bg-primary text-light" onClick={handleClick}>
+                <button id="new-quote" className=" btn btn-default bg-primary text-light" onClick={getQuote}>
                     New Quote
                 </button>
             </div>
